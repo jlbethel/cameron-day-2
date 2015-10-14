0d0838d944b749f0832b3e051ac0982ca05f7900
Jason Bethel <jlbethel@gmail.com>
2015-10-14 11:15:41 -0700

    add db

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -213,13 +213,13 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'cameron',
       'username' => 'admin',
-      'password' => 'adminPassw0rd',
+      'password' => 'adminPassword',
       'host' => '127.0.0.1',
       'port' => '8889',
       'driver' => 'mysql',
